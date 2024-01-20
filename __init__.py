ca20c8a0fff58125368512e913eaf2cf5df1aeb3
Ryan <dev.ryanmontes@gmail.com>
2024-01-19 19:50:03 -0800

    Add version 2.8.1

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "SNaP",
     # "author": "Ryan Montes",
-    "version": (2, 8, 0),
+    "version": (2, 8, 1),
     "blender": (3, 00, 0),
     "location": "Tools Shelf",
     "description": "SNaP",
