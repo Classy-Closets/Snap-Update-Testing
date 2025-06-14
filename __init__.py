7282bcf0bd5d08f09567fbb9b5fac34272512f3f
Ryan <RyanMDev@users.noreply.github.com>
2025-06-13 19:38:48 -0700

    Add version 3.2.0

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "SNaP",
     # "author": "Ryan Montes",
-    "version": (3, 0, 1),
+    "version": (3, 2, 0),
     "blender": (3, 00, 0),
     "location": "Tools Shelf",
     "description": "SNaP",
